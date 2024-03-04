18eb202e9877b71d310ec18fc05946b42683f056
GameDevChi <117703124+gdevchi@users.noreply.github.com>
2024-03-03 19:11:29 -0800

    Delete services directory

--- services/io.services.js
+++
@@ -1,9 +0,0 @@
-const { io } = require("../config/setup");
-
-exports.getRoomUsers = async (roomId) => {
-  return await io.in(roomId).fetchSockets();
-};
-
-exports.getRoomUserCounts = async (roomId) => {
-  return (await this.getRoomUsers(roomId)).length;
-};
