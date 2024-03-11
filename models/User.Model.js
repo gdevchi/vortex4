2076f9e39afac95a5589b9f4730a1f7691eaa6dc
GameDevChi <117703124+gdevchi@users.noreply.github.com>
2024-03-10 20:50:36 -0700

    Add files via upload

---
+++ models/User.Model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  account: {
+    id: String,
+    role: String,
+    label: String,
+  },
+  username: String,
+  userId: {
+    type: String,
+    required: [true, "Please provide socket Id"],
+  },
+  roomId: String,
+  position: {
+    x: Number,
+    y: Number,
+  },
+  speechDisabled: {
+    type: Boolean,
+    default: false,
+  },
+  assignedAt: Number,
+  status: {
+    type: String,
+    enum: {
+      values: ["joined", "waitlisted"],
+      message: "Please provide valid status",
+    },
+    default: "joined",
+  },
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
---
+++ public/js/login.js
@@ -0,0 +1,130 @@
+const messageEl = document.querySelector(".message");
+const metamaskBtn = document.querySelector(".metamask-button");
+const guestForm = document.querySelector(".guest-form");
+
+const loginState = {
+  progress: false,
+  ethereum: null,
+};
+
+function gotoPage() {
+  const redirectTo =
+    new URLSearchParams(window.location.search).get("redirectTo") ||
+    "/conferences";
+  window.location.href = redirectTo;
+}
+
+try {
+  const MMSDK = new MetaMaskSDK.MetaMaskSDK();
+  MMSDK.init()
+    .then(() => {
+      loginState.ethereum = MMSDK.getProvider();
+      metamaskBtn.classList.remove("disabled");
+      if (loginState.ethereum) {
+        loginState.ethereum.on("disconnect", () => {
+          console.log("disconnected!");
+        });
+        loginState.ethereum.on("error", () => {
+          console.log("error");
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+} catch (err) {
+  messageEl.textContent = err.message;
+}
+
+async function getUserNonce(address) {
+  const rawRes = await fetch("/api/v1/auth/nonce", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      address,
+    }),
+  });
+  const response = await rawRes.json();
+  if (response.status !== "success") throw response;
+  return response.data.nonce;
+}
+
+async function getAuthToken(crendentials) {
+  const rawRes = await fetch("/api/v1/auth/authenticate", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(crendentials),
+  });
+  const response = await rawRes.json();
+  if (response.status !== "authorized") throw response;
+}
+
+// Function to request user login using Metamask
+async function initiateLogin(ethereum) {
+  try {
+    loginState.progress = true;
+    const accounts = await ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    messageEl.textContent =
+      "Please open metamask app manually, if metamask sign window popup not visible in mobile";
+
+    const address = accounts[0];
+    const nonce = await getUserNonce(address);
+    const message = `I am signing this message with nonce:${nonce}`;
+    const signedMessage = await ethereum.request({
+      method: "personal_sign",
+      params: [message, address],
+    });
+    //get and save auth token in cookie
+    await getAuthToken({ signedMessage, message, address });
+    gotoPage();
+  } catch (err) {
+    loginState.progress = false;
+    alert(err.message);
+  }
+}
+
+async function getGuestAuthToken(crendentials) {
+  const rawRes = await fetch("/api/v1/auth/authenticate-guest", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(crendentials),
+  });
+  const response = await rawRes.json();
+  if (response.status !== "authorized") throw response;
+}
+
+// Handle the login button click
+metamaskBtn.addEventListener("click", async () => {
+  try {
+    if (!loginState.ethereum) {
+      throw { message: "Please wait initializing sdk" };
+    }
+    await initiateLogin(loginState.ethereum);
+  } catch (err) {
+    loginState.progress = false;
+    alert(err.message);
+  }
+});
+
+//Handle guest form button click
+guestForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  if (loginState.progress) return;
+  try {
+    loginState.progress = true;
+    await getGuestAuthToken({
+      username: event.target.elements.username.value,
+    });
+    gotoPage();
+  } catch (err) {
+    loginState.progress = false;
+    alert(err.message);
+  }
+});
